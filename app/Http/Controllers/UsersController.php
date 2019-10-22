ff7201ff6db4571041a6f0a3ef4d7c1c231f2a2b
Daouda Sanogo <jordavid1fci@gmail.com>
2019-10-22 03:23:23 +0200

    Added documentation to UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,20 +9,42 @@ class UsersController extends Controller
 {
     private $user;
 
+    /**
+     * Class constructor
+     *
+     * @param UserRepositoryInterface $user
+     */
     public function __construct(UserRepositoryInterface $user) {
         $this->user = $user;
     }
 
+    /**
+     * get all users
+     *
+     * @return users
+     */
     public function index()
     {
         return $this->user->all();
     }
 
+    /**
+     * Show a single user
+     *
+     * @param int $user_id
+     * @return user
+     */
     public function show($user_id)
     {
         return $this->user->getById($user_id);
     }
 
+    /**
+     * get filtered user by name
+     *
+     * @param string $name
+     * @return users
+     */
     public function getUsersByName($name)
     {
         return $this->user->getByName($name);
